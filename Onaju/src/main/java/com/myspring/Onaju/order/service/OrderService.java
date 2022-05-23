f1e6132be41424bef1df632aaba6bb1cfafc9d42
LeeHanBin <dlgksqls7218@naver.com>
2022-05-23 09:26:21 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/LukaBery/test.git
    
    Conflicts:
    	Onaju/target/m2e-wtp/web-resources/META-INF/maven/com.spring/mytest04/pom.properties

--- Onaju/src/main/java/com/myspring/Onaju/order/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.myspring.Onaju.order.service;
-
-import java.util.List;
-
-import com.myspring.Onaju.order.vo.OrderVO;
-
-public interface OrderService {
-	public void addNewOrder(OrderVO myOrderList) throws Exception;
-}
