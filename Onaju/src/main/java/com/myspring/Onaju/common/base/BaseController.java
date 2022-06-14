454b9b4e6000f877496f54ce7db9599cf8b5d42c
루하 <@DESKTOP-GNGDT2F>
2022-06-14 13:47:10 +0900

    베이스 컨트롤러 수정

--- Onaju/src/main/java/com/myspring/Onaju/common/base/BaseController.java
+++ Onaju/src/main/java/com/myspring/Onaju/common/base/BaseController.java
@@ -125,8 +125,12 @@ public abstract class BaseController  {
 	
 	@RequestMapping(value="/*.do" ,method={RequestMethod.POST,RequestMethod.GET})
 	protected  ModelAndView viewForm(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = new ModelAndView();
 		String viewName=(String)request.getAttribute("viewName");
-		ModelAndView mav = new ModelAndView(viewName);
+	
+
+	
+		mav.setViewName(viewName);
 		return mav;
 	}
 	
